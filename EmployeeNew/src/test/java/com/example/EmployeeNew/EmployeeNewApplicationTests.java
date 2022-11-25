c38b735ad1d6fb8ca53a9496fa802891f4c95a60
NaveenVetrivelS10 <116340872+NaveenVetrivelS10@users.noreply.github.com>
2022-11-25 19:52:20 +0530

    Add files via upload

---
+++ EmployeeNew/src/test/java/com/example/EmployeeNew/EmployeeNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
